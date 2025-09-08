33b06f97fcaa080a4d876659369bdc7ebc43808c
JoshuaGoldberg <joshua@barrygoldberg.net>
2025-09-08 15:36:05 -0400

    toy language completion

--- OwlLean/TestFiles/macros.lean
+++ OwlLean/TestFiles/macros.lean
@@ -12,19 +12,20 @@ inductive Expr : Nat → Type
 | Ident {Γ} : (i : Fin Γ) → Expr Γ
 | Ite : (g : Expr Γ) → (thn : Expr Γ) → (els : Expr Γ) → Expr Γ
 | Bind : (e1 : Expr Γ) → (e2 : Expr (Γ + 1)) → Expr Γ
+| Add : Expr Γ -> Expr Γ -> Expr Γ
 
 
 -- a TinyPPL parser and elaborator
 open Lean Elab Meta
 declare_syntax_cat tiny_lang
-syntax Parser.numLit : tiny_lang
+syntax num : tiny_lang
 syntax tiny_lang "←" tiny_lang ";" tiny_lang : tiny_lang
 syntax ident : tiny_lang
 syntax "true" : tiny_lang
 syntax "false" : tiny_lang
 syntax "if" tiny_lang "then" tiny_lang "else" tiny_lang : tiny_lang
 syntax "(" tiny_lang ")" : tiny_lang
-
+syntax tiny_lang "+" tiny_lang : tiny_lang
 
 inductive SExpr : Type
 | True : SExpr
@@ -33,6 +34,7 @@ inductive SExpr : Type
 | Ident : String → SExpr
 | Ite : (g : SExpr) → (thn : SExpr) → (els : SExpr) → SExpr
 | Bind : String -> (e1 : SExpr) → (e2 : SExpr) → SExpr
+| Add : SExpr -> SExpr -> SExpr
 
 def TCtx := List String
 
@@ -63,15 +65,13 @@ partial def elabTinylang : Syntax → MetaM Expr
     mkAppM ``SExpr.Ite #[elab_g, elab_thn, elab_els]
   | `(tiny_lang| $id:ident) =>
     mkAppM ``SExpr.Ident #[mkStrLit id.getId.toString]
+  | `(tiny_lang| $n:num) =>
+    mkAppM ``SExpr.NatLit #[mkNatLit n.getNat]
+  | `(tiny_lang| $e1:tiny_lang + $e2:tiny_lang) => do
+    let elab_e1 ← elabTinylang e1
+    let elab_e2 ← elabTinylang e2
+    mkAppM ``SExpr.Add #[elab_e1, elab_e2]
   | _ => throwUnsupportedSyntax
-
-elab "tinylang" "{" p:tiny_lang "}" : term => elabTinylang p
-
-#eval tinylang {
-  x ← true;
-  y ← false;
-  y
-}
 
 -- TODO 1: Finish and connect to above elaborator
 -- TODO 2: Extend to Owl language
@@ -81,9 +81,51 @@ def SExpr.elab (s : SExpr) (c : TCtx) : Option (Expr c.length) :=
     match TCtx.lookup c i with
     | .none => .none
     | .some j => Expr.Ident j
-  | .Bind s e1 e2 => .none
-    -- add s to c in the front; elab e1 under c; elab e2 under s::c
+  | .Bind s e1 e2 =>
+      match (SExpr.elab e1 c) with
+      | .none => .none
+      | .some e1' =>
+        match (SExpr.elab e2 (s::c)) with
+        | .none => .none
+        | .some e2' => .some (Expr.Bind e1' e2')
   | .True => .some (Expr.True)
   | .False => .some (Expr.False)
-  -- if/then/else is just recursive
-  | _ => .none
+  | .Ite g thn els =>
+    match (SExpr.elab g c) with
+    | .none => .none
+    | .some g' =>
+      match (SExpr.elab thn c) with
+      | .none => .none
+      | .some thn' =>
+        match (SExpr.elab els c) with
+        | .none => .none
+        | .some els' => .some (Expr.Ite g' thn' els')
+  | .Add e1 e2 =>
+    match (SExpr.elab e1 c) with
+    | .none => .none
+    | .some e1' =>
+      match (SExpr.elab e2 c) with
+      | .none => .none
+      | .some e2' => .some (Expr.Add e1' e2')
+  | .NatLit n => .some (Expr.NatLit n)
+
+def elabHelper (s : SExpr) : Expr 0 :=
+  match SExpr.elab s [] with
+  | .some e => e
+  | .none => Expr.False --dummy value
+
+elab "tinylang" "{" p:tiny_lang "}" : term => do
+  let sexprTerm : Expr <- elabTinylang p
+  let sVal : SExpr <-
+    (unsafe do
+      Meta.evalExpr SExpr (mkConst ``SExpr) sexprTerm)
+  match SExpr.elab sVal [] with
+  | .none =>
+      throwError "tinylang: ill-formed term"
+  | .some _ => mkAppM ``elabHelper #[sexprTerm]
+
+#eval tinylang {
+  x ← true;
+  y ← false;
+  if y then x else (2 + y)
+}
