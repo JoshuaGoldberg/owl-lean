e5a1f3b00d483102e373cb1adfcdbe13253d8096
JoshuaGoldberg <joshua@barrygoldberg.net>
2025-10-01 22:53:22 -0400

    Revert "I swear I just blinked the the lattice unaxiomatized on its own. How odd, huh?"
    This reverts commit 20036e817d52fc859dadd57833ba9979c05a5f1b.

--- OwlLean/TypeChecker/OwlElaborator.lean
+++ OwlLean/TypeChecker/OwlElaborator.lean
@@ -19,19 +19,19 @@ syntax owl_label "⊓" owl_label : owl_label
 syntax "$" term : owl_label
 syntax "(" owl_label ")" : owl_label
 
-partial def elabLabel (L : Lattice) : Syntax → MetaM Expr
-  | `(owl_label| ( $e:owl_label)) => elabLabel L e
+partial def elabLabel : Syntax → MetaM Expr
+  | `(owl_label| ( $e:owl_label)) => elabLabel e
   | `(owl_label| ⟨ $t:term ⟩ ) => do
       let t' ← Term.TermElabM.run' do
-        Term.elabTerm t (mkConst ``Owl.Lattice.labels)
+        Term.elabTerm t (mkConst ``Owl.Lcarrier)
       mkAppM ``SLabel.latl #[t']
   | `(owl_label| $e1:owl_label ⊔ $e2:owl_label) => do
-      let elab_e1 <- elabLabel L e1
-      let elab_e2 <- elabLabel L e2
+      let elab_e1 <- elabLabel e1
+      let elab_e2 <- elabLabel e2
       mkAppM ``SLabel.ljoin #[elab_e1, elab_e2]
   | `(owl_label| $e1:owl_label ⊓ $e2:owl_label) => do
-      let elab_e1 <- elabLabel L e1
-      let elab_e2 <- elabLabel L e2
+      let elab_e1 <- elabLabel e1
+      let elab_e2 <- elabLabel e2
       mkAppM ``SLabel.lmeet #[elab_e1, elab_e2]
   | `(owl_label| $id:ident) =>
     mkAppM ``SLabel.var_label #[mkStrLit id.getId.toString]
@@ -66,11 +66,11 @@ partial def elabCondSym : Syntax → MetaM Expr
 syntax "(" owl_constr ")" : owl_constr
 syntax owl_label owl_cond_sym owl_label : owl_constr
 
-partial def elabConstr (L : Lattice) : Syntax → MetaM Expr
-  | `(owl_constr| ( $e:owl_constr)) => elabConstr L e
+partial def elabConstr : Syntax → MetaM Expr
+  | `(owl_constr| ( $e:owl_constr)) => elabConstr e
   | `(owl_constr| $l1:owl_label $c:owl_cond_sym $l2:owl_label) => do
-      let elab_l1 <- elabLabel L l1
-      let elab_l2 <- elabLabel L l2
+      let elab_l1 <- elabLabel l1
+      let elab_l2 <- elabLabel l2
       let elab_c <- elabCondSym c
       mkAppM ``SConstr.condition #[elab_c, elab_l1, elab_l2]
   | _ => throwUnsupportedSyntax
@@ -98,7 +98,6 @@ syntax "(" owl_type ")" : owl_type
 syntax ident : owl_type
 syntax "Any" : owl_type
 syntax "Unit" : owl_type
-syntax "Public" : owl_type
 syntax "Data" owl_label : owl_type
 syntax "Ref" owl_type : owl_type
 syntax owl_type "->" owl_type : owl_type
@@ -110,48 +109,47 @@ syntax "∀" owl_label owl_cond_sym owl_label "." owl_type : owl_type
 syntax "if" owl_constr "then" owl_type "else" owl_type : owl_type
 syntax "$" term : owl_type
 
-partial def elabType (L : Lattice) : Syntax → MetaM Expr
-  | `(owl_type| ( $e:owl_type)) => elabType L e
+partial def elabType : Syntax → MetaM Expr
+  | `(owl_type| ( $e:owl_type)) => elabType e
   | `(owl_type| $id:ident) =>
         mkAppM ``STy.var_ty #[mkStrLit id.getId.toString]
   | `(owl_type| Any) => mkAppM ``STy.Any #[]
   | `(owl_type| Unit) => mkAppM ``STy.Unit #[]
-  | `(owl_type| Public) => mkAppM ``STy.Public #[]
   | `(owl_type| Data $l:owl_label) => do
-    let elab_l <- elabLabel L l
+    let elab_l <- elabLabel l
     mkAppM ``STy.Data #[elab_l]
   | `(owl_type| Ref $t:owl_type) => do
-    let elab_t <- elabType L t
+    let elab_t <- elabType t
     mkAppM ``STy.Ref #[elab_t]
   | `(owl_type| $t1:owl_type -> $t2:owl_type) => do
-    let elab_t1 <- elabType L t1
-    let elab_t2 <- elabType L t2
+    let elab_t1 <- elabType t1
+    let elab_t2 <- elabType t2
     mkAppM ``STy.arr #[elab_t1, elab_t2]
   | `(owl_type| $t1:owl_type * $t2:owl_type) => do
-    let elab_t1 <- elabType L t1
-    let elab_t2 <- elabType L t2
+    let elab_t1 <- elabType t1
+    let elab_t2 <- elabType t2
     mkAppM ``STy.prod #[elab_t1, elab_t2]
   | `(owl_type| $t1:owl_type + $t2:owl_type) => do
-    let elab_t1 <- elabType L t1
-    let elab_t2 <- elabType L t2
+    let elab_t1 <- elabType t1
+    let elab_t2 <- elabType t2
     mkAppM ``STy.sum #[elab_t1, elab_t2]
   | `(owl_type| ∀ $id:ident <: $t1:owl_type . $t2:owl_type) => do
-    let elab_t1 <- elabType L t1
-    let elab_t2 <- elabType L t2
+    let elab_t1 <- elabType t1
+    let elab_t2 <- elabType t2
     mkAppM ``STy.all #[mkStrLit id.getId.toString, elab_t1, elab_t2]
   | `(owl_type| ∃ $id:ident <: $t1:owl_type . $t2:owl_type) => do
-    let elab_t1 <- elabType L t1
-    let elab_t2 <- elabType L t2
+    let elab_t1 <- elabType t1
+    let elab_t2 <- elabType t2
     mkAppM ``STy.ex #[mkStrLit id.getId.toString, elab_t1, elab_t2]
   | `(owl_type| ∀ $id:ident $c:owl_cond_sym $l:owl_label . $t:owl_type) => do
-    let elab_t <- elabType L t
-    let elab_l <- elabLabel L l
+    let elab_t <- elabType t
+    let elab_l <- elabLabel l
     let elab_c <- elabCondSym c
     mkAppM ``STy.all_l #[mkStrLit id.getId.toString, elab_c, elab_l, elab_t]
   | `(owl_type| if $c:owl_constr then $t1:owl_type else $t2:owl_type) => do
-    let elab_t1 <- elabType L t1
-    let elab_t2 <- elabType L t2
-    let elab_c <- elabConstr L c
+    let elab_t1 <- elabType t1
+    let elab_t2 <- elabType t2
+    let elab_c <- elabConstr c
     mkAppM ``STy.t_if #[elab_c, elab_t1, elab_t2]
   | `(owl_type| $ $t:term) => do
     let t' ← Term.TermElabM.run' do
@@ -193,8 +191,8 @@ syntax "λ" ident "." owl_tm : owl_tm
 syntax "$" term : owl_tm
 syntax "(" owl_tm ":" owl_type ")" : owl_tm
 
-partial def elabTm (L : Lattice) : Syntax → TermElabM Expr
-  | `(owl_tm| ( $e:owl_tm)) => elabTm L e
+partial def elabTm : Syntax → TermElabM Expr
+  | `(owl_tm| ( $e:owl_tm)) => elabTm e
   | `(owl_tm| $id:ident) =>
         mkAppM ``SExpr.var_tm #[mkStrLit id.getId.toString]
   | `(owl_tm| $n:num) =>
@@ -205,104 +203,104 @@ partial def elabTm (L : Lattice) : Syntax → TermElabM Expr
     let elab_b <- elabBinary b
     mkAppM ``SExpr.bitstring #[elab_b]
   | `(owl_tm| fix $f:ident ( $id:ident ) $e:owl_tm) => do
-    let elab_e <- elabTm L e
+    let elab_e <- elabTm e
     mkAppM ``SExpr.fixlam #[mkStrLit f.getId.toString, mkStrLit id.getId.toString, elab_e]
   | `(owl_tm| Λ $id:ident . $e:owl_tm) => do
-    let elab_e <- elabTm L e
+    let elab_e <- elabTm e
     mkAppM ``SExpr.tlam #[mkStrLit id.getId.toString, elab_e]
   | `(owl_tm| Λβ $id:ident . $e:owl_tm) => do
-    let elab_e <- elabTm L e
+    let elab_e <- elabTm e
     mkAppM ``SExpr.l_lam #[mkStrLit id.getId.toString, elab_e]
   | `(owl_tm|⟨ $e1:owl_tm , $e2:owl_tm ⟩) => do
-    let elab_e1 <- elabTm L e1
-    let elab_e2 <- elabTm L e2
+    let elab_e1 <- elabTm e1
+    let elab_e2 <- elabTm e2
     mkAppM ``SExpr.tm_pair #[elab_e1, elab_e2]
   | `(owl_tm| ⟨ $t:term ⟩ ( $e1:owl_tm , $e2:owl_tm )) => do
     let t' ← Term.TermElabM.run' do
         Term.elabTerm t (mkConst ``Owl.op)
-    let elab_e1 <- elabTm L e1
-    let elab_e2 <- elabTm L e2
+    let elab_e1 <- elabTm e1
+    let elab_e2 <- elabTm e2
     mkAppM ``SExpr.Op #[t', elab_e1, elab_e2]
   | `(owl_tm| $ $t:term) => do
     let t' ← Term.TermElabM.run' do
         Term.elabTerm t (mkConst ``Owl.tm)
     mkAppM ``SExpr.embedtm #[t']
   | `(owl_tm| zero $e:owl_tm) => do
-    let elab_e <- elabTm L e
+    let elab_e <- elabTm e
     mkAppM ``SExpr.zero #[elab_e]
   | `(owl_tm| $e1:owl_tm [ $e2:owl_tm ]) => do
-    let elab_e1 <- elabTm L e1
-    let elab_e2 <- elabTm L e2
+    let elab_e1 <- elabTm e1
+    let elab_e2 <- elabTm e2
     mkAppM ``SExpr.app #[elab_e1, elab_e2]
   | `(owl_tm| alloc $e:owl_tm) => do
-    let elab_e <- elabTm L e
+    let elab_e <- elabTm e
     mkAppM ``SExpr.alloc #[elab_e]
   | `(owl_tm| ! $e:owl_tm) => do
-    let elab_e <- elabTm L e
+    let elab_e <- elabTm e
     mkAppM ``SExpr.dealloc #[elab_e]
   | `(owl_tm| $e1:owl_tm := $e2:owl_tm) => do
-    let elab_e1 <- elabTm L e1
-    let elab_e2 <- elabTm L e2
+    let elab_e1 <- elabTm e1
+    let elab_e2 <- elabTm e2
     mkAppM ``SExpr.assign #[elab_e1, elab_e2]
   | `(owl_tm| π1 $e:owl_tm) => do
-    let elab_e <- elabTm L e
+    let elab_e <- elabTm e
     mkAppM ``SExpr.left_tm #[elab_e]
   | `(owl_tm| π2 $e:owl_tm) => do
-    let elab_e <- elabTm L e
+    let elab_e <- elabTm e
     mkAppM ``SExpr.right_tm #[elab_e]
   | `(owl_tm| ı1 $e:owl_tm) => do
-    let elab_e <- elabTm L e
+    let elab_e <- elabTm e
     mkAppM ``SExpr.inl #[elab_e]
   | `(owl_tm| ı2 $e:owl_tm) => do
-    let elab_e <- elabTm L e
+    let elab_e <- elabTm e
     mkAppM ``SExpr.inr #[elab_e]
   | `(owl_tm| case $e1:owl_tm in $id1:ident => $e2:owl_tm | $id2:ident => $e3:owl_tm) => do
-    let elab_e1 <- elabTm L e1
-    let elab_e2 <- elabTm L e2
-    let elab_e3 <- elabTm L e3
+    let elab_e1 <- elabTm e1
+    let elab_e2 <- elabTm e2
+    let elab_e3 <- elabTm e3
     mkAppM ``SExpr.case #[elab_e1, mkStrLit id1.getId.toString, elab_e2, mkStrLit id2.getId.toString, elab_e3]
   | `(owl_tm| $e:owl_tm [[ $t:owl_type ]]) => do
-    let elab_e <- elabTm L e
-    let elab_t <- elabType L t
+    let elab_e <- elabTm e
+    let elab_t <- elabType t
     mkAppM ``SExpr.tapp #[elab_e, elab_t]
   | `(owl_tm| $e:owl_tm [[[ $l:owl_label ]]]) => do
-    let elab_e <- elabTm L e
-    let elab_l <- elabLabel L l
+    let elab_e <- elabTm e
+    let elab_l <- elabLabel l
     mkAppM ``SExpr.lapp #[elab_e, elab_l]
   | `(owl_tm| unpack $e1:owl_tm as ($id1:ident, $id2:ident) in $e2:owl_tm) => do
-    let elab_e1 <- elabTm L e1
-    let elab_e2 <- elabTm L e2
+    let elab_e1 <- elabTm e1
+    let elab_e2 <- elabTm e2
     mkAppM ``SExpr.unpack #[elab_e1, mkStrLit id1.getId.toString, mkStrLit id2.getId.toString, elab_e2]
   | `(owl_tm| pack ($t:owl_type, $e:owl_tm)) => do
-    let elab_t <- elabType L t
-    let elab_e <- elabTm L e
+    let elab_t <- elabType t
+    let elab_e <- elabTm e
     mkAppM ``SExpr.pack #[elab_t, elab_e]
   | `(owl_tm| if $e1:owl_tm then $e2:owl_tm else $e3:owl_tm) => do
-    let elab_e1 <- elabTm L e1
-    let elab_e2 <- elabTm L e2
-    let elab_e3 <- elabTm L e3
+    let elab_e1 <- elabTm e1
+    let elab_e2 <- elabTm e2
+    let elab_e3 <- elabTm e3
     mkAppM ``SExpr.if_tm #[elab_e1, elab_e2, elab_e3]
   | `(owl_tm| if $c:owl_constr then $e1:owl_tm else $e2:owl_tm) => do
-    let elab_c <- elabConstr L c
-    let elab_e1 <- elabTm L e1
-    let elab_e2 <- elabTm L e2
+    let elab_c <- elabConstr c
+    let elab_e1 <- elabTm e1
+    let elab_e2 <- elabTm e2
     mkAppM ``SExpr.if_c #[elab_c, elab_e1, elab_e2]
   | `(owl_tm| sync $e:owl_tm) => do
-    let elab_e <- elabTm L e
+    let elab_e <- elabTm e
     mkAppM ``SExpr.sync #[elab_e]
   | `(owl_tm| let $id1:ident = $e:owl_tm  in $b:owl_tm) => do
-    let elab_e <- elabTm L e
-    let elab_b <- elabTm L b
+    let elab_e <- elabTm e
+    let elab_b <- elabTm b
     let unused := "unused variable"
     let lambda <- mkAppM ``SExpr.fixlam #[mkStrLit unused, mkStrLit id1.getId.toString, elab_b]
     mkAppM ``SExpr.app #[lambda, elab_e]
   | `(owl_tm| λ $id:ident . $e:owl_tm) => do
-    let elab_e <- elabTm L e
+    let elab_e <- elabTm e
     let unused := "unused variable"
     mkAppM ``SExpr.fixlam #[mkStrLit unused, mkStrLit id.getId.toString, elab_e]
   | `(owl_tm| ( $e:owl_tm : $t:owl_type)) => do
-    let elab_e <- elabTm L e
-    let elab_t <- elabType L t
+    let elab_e <- elabTm e
+    let elab_t <- elabType t
     mkAppM ``SExpr.annot #[elab_e, elab_t]
   | _ => throwUnsupportedSyntax
 
@@ -449,7 +447,7 @@ partial def elabTm_closed : Syntax → TermElabM Expr
     mkAppM ``SExpr.tapp #[elab_e, elab_t]
   | `(owl_tm| $e:owl_tm [[[ $l:owl_label ]]]) => do
     let elab_e <- elabTm_closed e
-    let elab_l <- elabLabel_closed l
+    let elab_l <- elabLabel l
     mkAppM ``SExpr.lapp #[elab_e, elab_l]
   | `(owl_tm| unpack $e1:owl_tm as ($id1:ident, $id2:ident) in $e2:owl_tm) => do
     let elab_e1 <- elabTm_closed e1
--- OwlLean/TypeChecker/OwlSExpr.lean
+++ OwlLean/TypeChecker/OwlSExpr.lean
@@ -10,13 +10,13 @@ inductive SBinary : Type
 | bend : SBinary
 deriving Repr
 
-inductive SLabel (L : Lattice) : Type
-| var_label : String -> SLabel L
-| latl : L.labels -> SLabel L
-| ljoin : SLabel L-> SLabel L-> SLabel L
-| lmeet : SLabel L-> SLabel L-> SLabel L
-| embedlabel : Owl.label L 0 -> SLabel L
-| default : SLabel L
+inductive SLabel : Type
+| var_label : String -> SLabel
+| latl : Owl.Lcarrier -> SLabel
+| ljoin : SLabel -> SLabel -> SLabel
+| lmeet : SLabel -> SLabel -> SLabel
+| embedlabel : Owl.label 0 -> SLabel
+| default : SLabel
 deriving Repr
 
 inductive SCondSym : Type
@@ -30,59 +30,58 @@ inductive SCondSym : Type
 | nlt : SCondSym
 deriving Repr
 
-inductive SConstr (L : Lattice) : Type where
-| condition : SCondSym -> SLabel L-> SLabel L-> SConstr L
+inductive SConstr : Type where
+| condition : SCondSym -> SLabel -> SLabel -> SConstr
 deriving Repr
 
-inductive STy (L : Lattice) : Type where
-| var_ty :String -> STy L
-| Any : STy L
-| Unit : STy L
-| Data : SLabel L-> STy L
-| Ref : STy L -> STy L
-| arr : STy L -> STy L -> STy L
-| prod : STy L -> STy L -> STy L
-| sum : STy L -> STy L -> STy L
-| all : String -> STy L -> STy L -> STy L
-| ex : String -> STy L -> STy L -> STy L
-| all_l : String -> SCondSym -> SLabel L-> STy L -> STy L
-| t_if : SConstr L -> STy L -> STy L -> STy L
-| embedty : Owl.ty L 0 0 -> STy L
-| Public : STy L
-| default : STy L
+inductive STy : Type where
+| var_ty :String -> STy
+| Any : STy
+| Unit : STy
+| Data : SLabel -> STy
+| Ref : STy -> STy
+| arr : STy -> STy -> STy
+| prod : STy -> STy -> STy
+| sum : STy -> STy -> STy
+| all : String -> STy -> STy -> STy
+| ex : String -> STy -> STy -> STy
+| all_l : String -> SCondSym -> SLabel -> STy -> STy
+| t_if : SConstr -> STy -> STy -> STy
+| embedty : Owl.ty 0 0 -> STy
+| default : STy
 deriving Repr
 
-inductive SExpr (L : Lattice) : Type where
-| var_tm : String -> SExpr L
-| error : SExpr L
-| skip : SExpr L
-| bitstring : SBinary -> SExpr L
-| loc : Nat -> SExpr L
-| fixlam : String -> String -> SExpr L -> SExpr L
-| tlam : String -> SExpr L -> SExpr L
-| l_lam : String -> SExpr L -> SExpr L
-| Op : Owl.op -> SExpr L -> SExpr L -> SExpr L
-| zero : SExpr L -> SExpr L
-| app : SExpr L -> SExpr L -> SExpr L
-| alloc : SExpr L -> SExpr L
-| dealloc : SExpr L -> SExpr L
-| assign : SExpr L -> SExpr L -> SExpr L
-| tm_pair : SExpr L -> SExpr L -> SExpr L
-| left_tm : SExpr L -> SExpr L
-| right_tm : SExpr L -> SExpr L
-| inl : SExpr L -> SExpr L
-| inr : SExpr L -> SExpr L
-| case : SExpr L -> String -> SExpr L -> String -> SExpr L -> SExpr L
-| tapp : SExpr L -> STy L -> SExpr L
-| lapp : SExpr L -> SLabel L-> SExpr L
-| pack : STy L -> SExpr L -> SExpr L
-| unpack : SExpr L -> String -> String -> SExpr L -> SExpr L
+inductive SExpr : Type where
+| var_tm : String -> SExpr
+| error : SExpr
+| skip : SExpr
+| bitstring : SBinary -> SExpr
+| loc : Nat -> SExpr
+| fixlam : String -> String -> SExpr -> SExpr
+| tlam : String -> SExpr -> SExpr
+| l_lam : String -> SExpr -> SExpr
+| Op : Owl.op -> SExpr -> SExpr -> SExpr
+| zero : SExpr -> SExpr
+| app : SExpr -> SExpr -> SExpr
+| alloc : SExpr -> SExpr
+| dealloc : SExpr -> SExpr
+| assign : SExpr -> SExpr -> SExpr
+| tm_pair : SExpr -> SExpr -> SExpr
+| left_tm : SExpr -> SExpr
+| right_tm : SExpr -> SExpr
+| inl : SExpr -> SExpr
+| inr : SExpr -> SExpr
+| case : SExpr -> String -> SExpr -> String -> SExpr -> SExpr
+| tapp : SExpr -> STy -> SExpr
+| lapp : SExpr -> SLabel -> SExpr
+| pack : STy -> SExpr -> SExpr
+| unpack : SExpr -> String -> String -> SExpr -> SExpr
 | if_tm :
-    SExpr L -> SExpr L -> SExpr L -> SExpr L
+    SExpr -> SExpr -> SExpr -> SExpr
 | if_c :
-    SConstr L -> SExpr L -> SExpr L -> SExpr L
-| sync : SExpr L -> SExpr L
-| embedtm : Owl.tm L 0 0 0 -> SExpr L
-| annot : SExpr L -> STy L -> SExpr L
-| default : SExpr L
+    SConstr -> SExpr -> SExpr -> SExpr
+| sync : SExpr -> SExpr
+| embedtm : Owl.tm 0 0 0 -> SExpr
+| annot : SExpr -> STy -> SExpr
+| default : SExpr
 deriving Repr
