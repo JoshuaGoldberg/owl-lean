ffc95e1faa900368ac0457d282feb2f819fb9847
JoshuaGoldberg <joshua@barrygoldberg.net>
2025-10-27 13:35:20 -0400

    moving notation around

--- OwlLean/TypeChecker/OwlParser.lean
+++ OwlLean/TypeChecker/OwlParser.lean
@@ -791,3 +791,39 @@ elab "(" p:owl_phi ";" ps:owl_psi ";" d:owl_delta ";" g:owl_gamma ";" e:owl_tm "
   let tmExpr ← mkAppM ``elabHelper #[← elabTm e, lvarsExpr, tvarsExpr, varsExpr]
 
   mkAppM ``has_type #[phiExpr, psiExpr, deltaExpr, gammaExpr, tmExpr, tyExpr]
+
+open Lean PrettyPrinter Delaborator SubExpr
+
+set_option pp.notation true
+set_option pp.fullNames false
+set_option pp.universes false
+set_option pp.proofs false
+
+notation:50 "<" Φ ", " Ψ " ⊨ " co ">" => phi_psi_entail_corr Φ Ψ co
+notation h "::" t => pcons h t
+notation "Ψ∅" => empty_phi 0
+notation "Φ∅" => empty_psi 0
+notation "Ψ∅" => empty_phi
+notation "Φ∅" => empty_psi
+notation "Δ∅" => empty_delta
+notation "Γ∅" => empty_gamma
+notation "corr(" l ")"  => Owl.corruption.corr l
+notation "¬corr(" l ")" => Owl.corruption.not_corr l
+notation pm " ▷ " ψ => subst_psi_context pm ψ
+notation "⟪" l "⟫" => Owl.label.latl l
+notation "⊑" => Owl.cond_sym.leq
+notation "⊒" => Owl.cond_sym.geq
+notation "⊏" => Owl.cond_sym.lt
+notation "⊐" => Owl.cond_sym.gt
+notation "ℓ" n => Owl.label.var_label n
+notation "(" a c b ")" => Owl.constr.condition c a b
+notation "↑" a => lift_phi a
+notation "↑" a => lift_psi a
+notation "[" a "]" => Owl.ren_label id a
+notation h "::" t => Owl.cons h t
+notation "⊥" => Owl.L.bot
+
+@[app_unexpander Owl.Lattice.bot]
+def unexpOwlBot : Unexpander
+| `($_ $_) => `(⊥)
+| _        => throw ()
