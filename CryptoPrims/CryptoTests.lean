0b1f0f88e765290dbf34365aba26b9eaf52175df
JoshuaGoldberg <joshua@barrygoldberg.net>
2026-02-25 10:38:42 -0500

    minor work

--- CryptoPrims/CryptoTests.lean
+++ CryptoPrims/CryptoTests.lean
@@ -56,7 +56,6 @@ def ENC_Inner := OwlTy [lK] [tM, tK] {
     ( tK *
         ((corr (lK) ? (Public * Public) -> Public : (tK * tM) -> Public) *
         (corr (lK) ? (Public * Public) -> (Public + unit) : (tK * Public) -> (tM + unit))))
-
 }
 
 def ENC := OwlTy [ lK ] [ tM ] {
@@ -216,7 +215,7 @@ def run_two_tm := Owl [] [] [] {
 }
 
 -- assume eq is a default function
-def alice_sm := Owl [lM, lKL, lKH] [] [encH, encL, msg] {
+def alice_sm := Owl [lKL, lKH] [] [encH, encL, msg] {
   (pack (Public,
     ⟨"0",
       (λ (args : (Public * Public)) : (Public * Public) =>
@@ -237,7 +236,7 @@ def alice_sm := Owl [lM, lKL, lKH] [] [encH, encL, msg] {
   : $ StateMachine [] [])
 }
 
-def bob_sm := Owl [lM, lKL, lKH] [] [encH, encL] {
+def bob_sm := Owl [lKL, lKH] [] [encH, encL] {
   (pack (Public,
     ⟨"0",
       (λ (args : (Public * Public)) : (Public * Public) =>
@@ -269,9 +268,9 @@ def bob_sm := Owl [lM, lKL, lKH] [] [encH, encL] {
 }
 
 def run_alice_bob :=
- Owl [lM, lKL, lKH] [] [encH, encL, msg] {
-  let a = ($ alice_sm [lM, lKL, lKH] [] [encH, encL, msg]) in
-  let b = ($ bob_sm [lM, lKL, lKH] [] [encH, encL]) in
+ Owl [lKL, lKH] [] [encH, encL, msg] {
+  let a = ($ alice_sm [lKL, lKH] [] [encH, encL, msg]) in
+  let b = ($ bob_sm [lKL, lKH] [] [encH, encL]) in
   ((($ run_two_tm [] [] [] : $ two_sm [] []) a) b)
  }
 
@@ -304,7 +303,7 @@ def run_alice_bob :=
   encL => ($ ENC_Inner [lKL] [Data lM, aKL]),
   msg => Data lM
   ⊢
-  $ alice_sm [lM, lKL, lKH] [] [encH, encL, msg]
+  $ alice_sm [lKL, lKH] [] [encH, encL, msg]
   :
   $ StateMachine [] []
   by {
@@ -317,7 +316,7 @@ def run_alice_bob :=
   encH => ($ ENC_Inner [lKH] [aKL, aKH]),
   encL => ($ ENC_Inner [lKL] [Data lM, aKL])
   ⊢
-  $ bob_sm [lM, lKL, lKH] [] [encH, encL]
+  $ bob_sm [lKL, lKH] [] [encH, encL]
   :
   $ StateMachine [] []
   by {
@@ -326,13 +325,13 @@ def run_alice_bob :=
     grind
   }
 
-
+-- the whole double state machine
 #tc tc_run_alice_bob := lM, lKL ⊐ lM, lKH ⊐ lKL ; · ; aKH <: Data lKH, aKL <: Data lKL ;
   encH => ($ ENC_Inner [lKH] [aKL, aKH]),
   encL => ($ ENC_Inner [lKL] [Data lM, aKL]),
   msg => Data lM
   ⊢
-  ($ run_alice_bob [lM, lKL, lKH] [] [encH, encL, msg])
+  ($ run_alice_bob [lKL, lKH] [] [encH, encL, msg])
   :
   (Public * Public) -> Public
   by {
