692ed2d062a5fb44ac06f5b73e08e5e2d77d2d8c
JoshuaGoldberg <joshua@barrygoldberg.net>
2026-04-01 00:59:53 -0400

    ideas

--- CryptoPrims/CryptoTests.lean
+++ CryptoPrims/CryptoTests.lean
@@ -461,9 +461,9 @@ def party_type :=
     λ send =>
     λ recv =>
       let enc_psk = π1 (π2 encPSK) in
-      let psk     = π1 encPSK       in
-      let dec_kx  = π2 (π2 encKX)  in
-      let key_x   = π1 encKX        in
+      let psk = π1 encPSK in
+      let dec_kx = π2 (π2 encKX) in
+      let key_x = π1 encKX in
       let ct1 = corr_case lKH in (enc_psk ⟨psk, key_x⟩) in
       (send ct1) (λ (_ : unit) : unit =>
         recv (λ (ct2 : Public) : unit =>
@@ -477,9 +477,8 @@ def party_type :=
     λ send =>
     λ recv =>
       let dec_psk = π2 (π2 encPSK) in
-      let enc_kx  = π1 (π2 encKX)  in
-      let psk     = π1 encPSK       in
-
+      let enc_kx = π1 (π2 encKX) in
+      let psk = π1 encPSK in
       recv (λ (ct1 : Public) : unit =>
         corr_case lKH in
         case (dec_psk ⟨psk, ct1⟩) with
@@ -492,15 +491,15 @@ def party_type :=
   -- run protocol
   λ (_ : unit) : (Public * Public) -> Public =>
     let s2c_k   : (Ref (Public -> unit)) = alloc (λ (_ : Public) : unit => ()) in
-    let s2c_out : (Ref Public)           = alloc ("" : Public) in
-    let c2s_k   : (Ref (Public -> unit)) = alloc (λ (_ : Public) : unit => ()) in
-    let c2s_out : (Ref Public)           = alloc ("" : Public) in
+    let s2c_out : (Ref Public) = alloc ("" : Public) in
+    let c2s_k : (Ref (Public -> unit)) = alloc (λ (_ : Public) : unit => ()) in
+    let c2s_out : (Ref Public) = alloc ("" : Public) in
 
     let send_server : (Public -> (unit -> unit) -> unit) =
       λ (v : Public) : ((unit -> unit) -> unit) =>
       λ (k : (unit -> unit)) : unit =>
         (s2c_out := v) ;
-        (s2c_k   := (λ (_ : Public) : unit => k ())) in
+        (s2c_k := (λ (_ : Public) : unit => k ())) in
 
     let recv_server : (Public -> unit) -> unit =
       λ (k : (Public -> unit)) : unit =>
@@ -514,13 +513,10 @@ def party_type :=
       λ (v : Public) : ((unit -> unit) -> unit) =>
       λ (k : (unit -> unit)) : unit =>
         (c2s_out := v) ;
-        (c2s_k   := (λ (_ : Public) : unit => k ())) in
+        k () in
 
     ((server send_server) recv_server) ;
-
-    ((!s2c_k) (!s2c_out)) ;
-
-    ((!c2s_k) (!c2s_out)) ;
+    ((client send_client) recv_client) ;
 
     (λ (args : (Public * Public)) : Public =>
       let (b, v) = args in
