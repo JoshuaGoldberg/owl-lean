707166353d40cef58f57ef0516982ebbd2fc97cd
JoshuaGoldberg <joshua@barrygoldberg.net>
2026-04-08 10:44:45 -0400

    test

--- CryptoPrims/CryptoTests.lean
+++ CryptoPrims/CryptoTests.lean
@@ -458,39 +458,40 @@ def party_type :=
   encKX => ($ ENC_Inner [lKL] [aX, aKX]) ,
   x => aX
   ⊢
-  let server : ($ party_type [] [unit]) =
-    λ send =>
-    λ recv =>
-      let enc_psk = π1 (π2 encPSK) in
-      let psk = π1 encPSK in
-      let dec_kx = π2 (π2 encKX) in
-      let key_x = π1 encKX in
-      let ct1 = corr_case lKH in (enc_psk ⟨psk, key_x⟩) in
-      (send ct1) (λ (_ : unit) : unit =>
-        recv (λ (ct2 : Public) : unit =>
-          corr_case lKL in
-          case (dec_kx ⟨key_x, ct2⟩) with
-          | inl _ => ()
-          | inr _ => ())) in
-
-
-  let client : ($ party_type [] [unit]) =
-    λ send =>
-    λ recv =>
-      let dec_psk = π2 (π2 encPSK) in
-      let enc_kx = π1 (π2 encKX) in
-      let psk = π1 encPSK in
-      recv (λ (ct1 : Public) : unit =>
-        corr_case lKH in
-        case (dec_psk ⟨psk, ct1⟩) with
-        | inl key_x' =>
-            let ct2 = corr_case lKL in (enc_kx ⟨key_x', x⟩) in
-            (send ct2) (λ (_ : unit) : unit => ())
-        | inr _ => ()) in
-
-
   -- run protocol
   λ (_ : unit) : (Public * Public) -> Public =>
+    let server_result : Ref (unit + (corr (lKL) ? Public : aX)) =
+      alloc (ı1 () : unit + (corr (lKL) ? Public : aX)) in
+
+    let server : ($ party_type [] [unit]) =
+      λ send =>
+      λ recv =>
+        let enc_psk = π1 (π2 encPSK) in
+        let psk = π1 encPSK in
+        let dec_kx = π2 (π2 encKX) in
+        let key_x = π1 encKX in
+        let ct1 = corr_case lKH in (enc_psk ⟨psk, key_x⟩) in
+        (send ct1) (λ (_ : unit) : unit =>
+          recv (λ (ct2 : Public) : unit =>
+            corr_case lKL in
+            case (dec_kx ⟨key_x, ct2⟩) with
+            | inl v => server_result := (ı2 v : unit + (corr (lKL) ? Public : aX))
+            | inr _ => ())) in
+
+    let client : ($ party_type [] [unit]) =
+      λ send =>
+      λ recv =>
+        let dec_psk = π2 (π2 encPSK) in
+        let enc_kx = π1 (π2 encKX) in
+        let psk = π1 encPSK in
+        recv (λ (ct1 : Public) : unit =>
+          corr_case lKH in
+          case (dec_psk ⟨psk, ct1⟩) with
+          | inl key_x' =>
+              let ct2 = corr_case lKL in (enc_kx ⟨key_x', x⟩) in
+              (send ct2) (λ (_ : unit) : unit => ())
+          | inr _ => ()) in
+
     let s2c_k   : (Ref (Public -> unit)) = alloc (λ (_ : Public) : unit => ()) in
     let s2c_out : (Ref Public) = alloc ("" : Public) in
     let c2s_k : (Ref (Public -> unit)) = alloc (λ (_ : Public) : unit => ()) in
