c8cc88e9a2206b575cf702ec570f883d7c13ae98
gancherj <j.gancher@northeastern.edu>
2026-05-26 11:23:04 -0400

    update enc multi

--- OwlLean/CryptoPrims/MultiEnc.lean
+++ OwlLean/CryptoPrims/MultiEnc.lean
@@ -6,9 +6,8 @@ import OwlLean.CryptoPrims.Utils
 
 
 #ty ENC_MULTI [lK, lM] [] [tau] :=
-    (∃ alphaK <: Any. {
+    (∃ alphaK <: Data lK . {
       genKey : (unit -> alphaK),
-      getKey : (alphaK -> Data lK),
       enc : if corr (lK) then (Public * Public) -> Public else (alphaK * tau) -> Public,
       dec : if corr (lK) then (Public * Public) -> (Public + unit) else (alphaK * Public) -> (tau + unit)
     })
@@ -16,7 +15,6 @@ import OwlLean.CryptoPrims.Utils
 #tc ENC_MULTI_REAL [lK ⊒ ⊥, lM ⊏ lK, corr (lK)] [] [tau <: Data lM] [] :=  ⊢ {
     pack (Data lK, {
       genKey := λ (_ : unit) : Data lK => ⟦genKey⟧(sample secparam),
-      getKey := λ (k : Data lK) : Data lK => k,
       enc := λ (x : (Public * Public)) : Public =>
         let (a, b) = x in
         let enc_rnd = sample (⟦enc_rand_bits⟧(secparam)) in
@@ -34,26 +32,21 @@ import OwlLean.CryptoPrims.Utils
         $ ENC_MULTI_REAL [lK, lM] [] [tau] []
     else  (
         let L = mk_map tau in
-        type alphaK = Public * Data lK  in
-        let genKey  = λ (_ : unit) : alphaK => ⟨⟦genKey⟧(sample secparam), sample secparam⟩  in
-        -- TODO: This doesn't work because maps currently cannot hold secret keys.
-        let enc = λ (x : (alphaK * tau)) : Public =>
+        let genKey  = λ (_ : unit) : Public => ⟦genKey⟧(sample secparam) in
+        let enc = λ (x : (Public * tau)) : Public =>
            let (k, m) = x in
-           let (hdl, k) = k in
            let c = sample (zero (m : Data lM)) in
-           set_map tau L (⟦concat⟧(hdl, c)) m;
+           set_map tau L (⟦concat⟧(k, c)) m;
            c
         in
-        let dec = λ (x : (alphaK * Public)) : (tau + unit) =>
+        let dec = λ (x : (Public * Public)) : (tau + unit) =>
           let (k, c) = x in
-          let (hdl, k) = k in
-          case get_map tau L (⟦concat⟧(hdl, c)) with
+          case get_map tau L (⟦concat⟧(k, c)) with
           | inl m => ı1 m
           | inr _ => ı2 ()
         in
-        pack (alphaK, {
+        pack (Public, {
           genKey := genKey,
-          getKey := λ (k : alphaK) : Data lK => let (_, k) = k in k,
           enc := enc,
           dec := dec
         })
